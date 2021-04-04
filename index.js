bf77ddfd5b7696d3e70ddfcb4436f3a6203c73c5
YesSeri <henrik.zenkert@gmail.com>
2021-04-04 19:03:56 +0200

    very basic visual representation

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const app = express();
 const PORT = 3000;
 
 app.use('/', express.static(path.join(__dirname, 'static')))
+app.use('/audio', express.static(path.join(__dirname, 'audio')))
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
