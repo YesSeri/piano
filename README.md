81ab11900cde49293f2e8c1cd1529bc69d711d53
yesseri <henrik.zenkert@gmail.com>
2021-04-08 17:11:30 +0200

    removed a lot to make room for the new canvas implementation

--- README.md
+++ README.md
@@ -24,6 +24,10 @@ This piano can be played with the keyboard.
 
 It can also be played by clicking, both on the computer and on a tablet, or phone. 
 
+## Future plans
+
+I want to make the keyboard more responsive. I can do this by drawing it to a canvas instead of using divs. I also need to improve the sounds. I can do that with the toneJS library, so that the sound is syntheziesed live instead of using real audio files. This repo might also help. [Repo](https://github.com/nbrosowsky/tonejs-instruments)
+
 ## Getting Started
 
 Just `git clone https://github.com/YesSeri/piano` and use yarn start to run it. You need to have nodemon installed globally. 
--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,15 @@
 "use strict";
 // The PianoKey class contains every relevant information and thing to create a piano key and play it.
 class PianoKey {
-    constructor(shortcut, audio, div, sharp) {
+    constructor(shortcut, audio, sharp) {
         this.audio = audio
-        this.div = div
         this.timer = null
         this.shortcut = shortcut
         this.pressed = false
         this.sharp = sharp
     }
     print() {
-        console.log({ 'div': this.div.id }, { 'timer': this.timer }, { 'shortcut': this.shortcut }, { 'pressed': this.pressed }, { 'sharp': this.sharp })
+        console.log( { 'timer': this.timer }, { 'shortcut': this.shortcut }, { 'pressed': this.pressed }, { 'sharp': this.sharp })
     }
     // Clears timeout to stop the fading out, and starts the sound from the beginning with max volume.
     playNote() {
@@ -39,104 +38,19 @@ class PianoKey {
 
 // Creates two arrays which will be used to create pianoKey objects that we will use in our piano. 
 const audios = Array.from(document.getElementsByClassName('key-sound'));
-const pianoKeyDivs = Array.from(document.getElementsByClassName('piano-key'));
 
 // This is used to translate between id of key div and audio id. 
 // This is only used when creating a pianoKey, because both the audio and the corresponding div gets added to the pianoKey object, so it will always be easy to find the corresponding one after that.
 
-let translations = {};
-for (const keyDiv of pianoKeyDivs) {
-    const splitter = str => str.split('-').slice(1).join('-')
-    const splitKeyId = splitter(keyDiv.id)
-    for (const audio of audios) {
-        const splitAudioId = splitter(audio.id)
-        if (splitAudioId === splitKeyId) {
-            translations[keyDiv.id] = audio.id
-            break;
-        }
-    }
-}
-
-const pianoKeys = pianoKeyDivs.map(div => {
-    const aud = audios.find(el => el.id === translations[div.id])
-    return new PianoKey(div.dataset.shortcut, aud, div, div.className.includes('sharp'))
+const pianoKeys = audios.map(aud => {
+    const shortcut  = aud.dataset.shortcut
+    return new PianoKey(shortcut, aud, aud.className.includes('sharp'))
 })
+console.log(pianoKeys);
 
 // When releasing mouse button we want to stop the last note played, not the one which the mouse is over when the button released. 
 // If you move mouse after pressing key you could release pointer over a new key. It should still be the played key that gets stopped. 
-let lastClicked;
-for (const pianoKey of pianoKeys) {
-    const { div, sharp } = pianoKey
-    if (!sharp) {
-        div.addEventListener("mousedown", (e) => {
-            findKeyById(key => {
-                key.playNote()
-                lastClicked = key;
-            }, e)
-        })
 
-    }
-}
-
-window.addEventListener("mouseup", (e) => {
-    // lastClicked is undefined, if no piano note has yet been clicked.
-    if (!lastClicked) return
-    lastClicked.stopNote()
-})
-
-window.addEventListener("DOMContentLoaded", e => {
-    const pianoDiv = document.getElementById('piano')
-    pianoDiv.addEventListener("touchstart", handleTouchStart, false);
-    pianoDiv.addEventListener("touchend", handleTouchEnd, false);
-    //pianoDiv.addEventListener("touchcancel", handleTouchCancel, false);
-    //pianoDiv.addEventListener("touchmove", handleTouchMove, false);
-})
-function handleTouchStart(e) {
-    e.preventDefault();
-    findKeyById(key => {
-        key.playNote()
-    }, e)
-}
-function handleTouchEnd(e) {
-    e.preventDefault();
-    findKeyById(key => {
-        key.stopNote()
-    }, e)
-}
-function handleTouchCancel(e) {
-}
-function handleTouchMove(e) {
-}
-
-window.addEventListener("keydown", e => {
-    // If you press a key while holding down a different one the next event of the key that is hold down will have e.repeat = true. This is why we must have a key.pressed variable.
-    if (e.repeat) return
-    // This finds the key pressed and then you can run a function on that key. We also need to send along the event.
-    findKeyByShortcut(key => {
-        if (key.pressed !== true) key.playNote()
-        key.pressed = true
-    }, e)
-})
-
-window.addEventListener("keyup", e => {
-    findKeyByShortcut(key => {
-        key.stopNote()
-        key.pressed = false
-    }, e)
-})
-
-// Finds the correct key in the pianoKeys array, and then a function is run on that key.
-function findKeyByShortcut(fn, event) {
-    for (const key of pianoKeys) {
-        if (event.key === key.shortcut) fn(key)
-    }
-}
-
-function findKeyById(fn, event) {
-    for (const key of pianoKeys) {
-        if (event.target.id === key.div.id) fn(key)
-    }
-}
 
 const kbBtn = document.getElementById('keybindings-btn')
 const kbDiv = document.getElementById('keybindings-div')
