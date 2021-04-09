adba739469148e56a3673fdb4e0323ef3eee9a69
YesSeri <henrik.zenkert@gmail.com>
2021-04-09 22:35:45 +0200

    Got tonejs working

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+var cors = require('cors')
+app.use(cors())
 
-app.use('/', express.static(path.join(__dirname, 'static')))
+app.use('/audio', express.static(path.join(__dirname, 'static', 'audio')));
+// app.use('/', express.static(path.join(__dirname, 'static', 'dist')))
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,31 @@
 "use strict";
 // The PianoKey class contains every relevant information and thing to create a piano key and play it.
+
+import * as Tone from 'tone'
+const testBtn = document.getElementById('test-btn')
+const osc = new Tone.Oscillator(440, "sine").toDestination()
+const sampler = new Tone.Sampler({
+    urls: {
+        C2: "C2.ogg",
+        "D#2": "Ds2.ogg",
+        "F#2": "Fs2.ogg",
+        A2: "A2.ogg",
+        C3: "C3.ogg",
+    },
+    release: 1,
+    baseUrl: "http://localhost:3000/audio/"
+}).toDestination();
+console.log(sampler);
+const note = 'D2'
+testBtn.addEventListener('mousedown', () => {
+    sampler.triggerAttack(note)
+    console.log('click')
+})
+
+testBtn.addEventListener('mouseup', () => {
+    sampler.triggerRelease(note)
+    console.log('rel')
+})
 class PianoKey {
     constructor(shortcut, audio, sharp) {
         this.audio = audio
@@ -9,7 +35,7 @@ class PianoKey {
         this.sharp = sharp
     }
     print() {
-        console.log( { 'timer': this.timer }, { 'shortcut': this.shortcut }, { 'pressed': this.pressed }, { 'sharp': this.sharp })
+        console.log({ 'timer': this.timer }, { 'shortcut': this.shortcut }, { 'pressed': this.pressed }, { 'sharp': this.sharp })
     }
     // Clears timeout to stop the fading out, and starts the sound from the beginning with max volume.
     playNote() {
@@ -43,10 +69,9 @@ const audios = Array.from(document.getElementsByClassName('key-sound'));
 // This is only used when creating a pianoKey, because both the audio and the corresponding div gets added to the pianoKey object, so it will always be easy to find the corresponding one after that.
 
 const pianoKeys = audios.map(aud => {
-    const shortcut  = aud.dataset.shortcut
+    const shortcut = aud.dataset.shortcut
     return new PianoKey(shortcut, aud, aud.className.includes('sharp'))
 })
-console.log(pianoKeys);
 
 // When releasing mouse button we want to stop the last note played, not the one which the mouse is over when the button released. 
 // If you move mouse after pressing key you could release pointer over a new key. It should still be the played key that gets stopped. 
