df58aa5b0d260f43543e24e5ae80131ea1d5e87d
yesseri <henrik.zenkert@gmail.com>
2021-04-10 12:40:13 +0200

    added dist folder for pushing to heroku

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-var cors = require('cors')
-app.use(cors())
+
 app.use('/audio', express.static(path.join(__dirname, 'static', 'audio')));
-app.use('/', express.static(path.join(__dirname, 'dist')))
+app.use('/', express.static(path.join(__dirname, 'static', 'dist')))
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
