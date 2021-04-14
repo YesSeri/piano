e61b693819b0543a19d1d5d37d413a8b88bbc7a5
yesseri <henrik.zenkert@gmail.com>
2021-04-14 14:29:08 +0200

    Fixed getting wifi bug. Getting wifi address needs to be done locally, or more genereally.

--- server.js
+++ server.js
@@ -12,14 +12,7 @@ app.use('/about', (req, res) => {
 })
 app.use('/', express.static(path.join(__dirname, 'static', 'dist')))
 
-const { address } = (() => {
-  const networkInterfaces = os.networkInterfaces();
-  for (netInfo of networkInterfaces['Wi-Fi']) {
-    if (netInfo['family'] === 'IPv4') return netInfo
-  }
-  throw new Error("Could not find wifi info, IPv4")
-})();
-
 app.listen(PORT, () => {
-  console.log(`Network Information \nWiFi: ${address}:${PORT}\nLocal: 127.0.0.1:${PORT}`);
+   //console.log(`Network Information \nWiFi: ${address}:${PORT}\nLocal: 127.0.0.1:${PORT}`);
+   console.log(`Network Information \nLocal: 127.0.0.1:${PORT}`);
 })
