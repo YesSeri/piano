93ffa6a13e2c310c5b8102d81cc6d3716896cb87
yesseri <henrik.zenkert@gmail.com>
2021-04-20 00:06:37 +0200

    completed a nice title and header

--- react/piano/src/component/Header.js
+++ react/piano/src/component/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import svg from '../assets/painted-piano.svg'
+import { ReactComponent as GrandPiano } from '../assets/grand-piano.svg'
 
 const Header = () => {
+    console.log(GrandPiano);
     return (
         <div className="header__bg">
-        <div className="header">
-            <div className="header__pane">
-                Your site for playing piano online.
-                Use the piano with keyboard, touch or mouse.
+            <div className="header">
+                <div className="header__paneLeft">
+                    <h2>Play the piano</h2>
+                    <p>Your site for playing piano online.
+                    Use the piano with keyboard, touch or mouse.</p>
+                </div>
+                <div className="header__paneRight">
+                    <GrandPiano fill='white'></GrandPiano>
+                </div>
             </div>
-            <div className="header__pane">
-                <img src={svg}></img>
-            </div>
-        </div>
         </div >
     )
 }
