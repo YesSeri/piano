a1733f8d7e2a1cf236cb789f9afb20471551f207
yesseri <henrik.zenkert@gmail.com>
2021-04-25 23:04:24 +0200

    improved css keybindings somewhat

--- src/component/Title.js
+++ src/component/Title.js
@@ -1,13 +1,10 @@
-import React from 'react'
-
 const Title = () => {
 
     return (<div className='title'>
         <h1 className='title__text'>
-            Online Piano
+            Pianisto
         </h1>
     </div>)
 }
-
 
 export default Title
