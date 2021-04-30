c0f0975124f8486f2b7be4b4d18a0809bb79e9f7
YesSeri <henrik.zenkert@gmail.com>
2021-04-30 11:35:17 +0200

    improved sound of upper notes

--- src/containers/PianoContainer.js
+++ src/containers/PianoContainer.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Sampler, Buffer } from 'tone'
-import { g3, a3, c4, ds4, fs4, a4, c5, ds5 } from '../assets/audio'
+import { g3, a3, c4, ds4, fs4, a4, c5, ds5, fs5, a5, c6, ds6, fs6 } from '../assets/audio'
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import Piano from '../component/piano'
 const g3b = new Buffer(g3)
@@ -11,6 +11,13 @@ const fs4b = new Buffer(fs4)
 const a4b = new Buffer(a4)
 const c5b = new Buffer(c5)
 const ds5b = new Buffer(ds5)
+
+const fs5b = new Buffer(fs5)
+const a5b = new Buffer(a5)
+const c6b = new Buffer(c6)
+const ds6b = new Buffer(ds6)
+const fs6b = new Buffer(fs6)
+
 const Container = ({ high, low, children, ...props }) => {
   const [loaded, setLoaded] = useState(false)
   const handle = useFullScreenHandle();
@@ -24,6 +31,11 @@ const Container = ({ high, low, children, ...props }) => {
       "A2": a4b,
       "C3": c5b,
       "D#3": ds5b,
+      "F#3": fs5b,
+      "A3": a5b,
+      "C4": c6b,
+      "D#4": ds6b,
+      "F#4": fs6b,
     },
     baseUrl: "../../assets/audio",
     release: 1,
